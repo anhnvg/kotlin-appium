619dad654bf01b6cfb0dd8475173d713aaa0864f
nv.anhmai <anhmai@nvg.vn>
2022-11-16 15:02:01 +0700

    fix: update the appium server address

--- src/main/kotlin/framework/AppiumServerManager.kt
+++ src/main/kotlin/framework/AppiumServerManager.kt
@@ -2,5 +2,5 @@ package framework
 import util.EnvironmentManager.isRemoteAppiumServer
 
 object AppiumServerManager {
-    val serverAddress = if (isRemoteAppiumServer) "<IP>:<PORT>/wd/hub" else "http://127.0.0.1:4723/wd/hub"
+    val serverAddress = if (isRemoteAppiumServer) "<IP>:<PORT>/wd/hub" else "http://127.0.0.1:4723/"
 }
