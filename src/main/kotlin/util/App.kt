dd9ccf2505c8d6bf4d4103b1f6ee0acd88a8515b
nv.anhmai <anhmai@nvg.vn>
2022-12-29 13:08:34 +0700

    fix: update

--- src/main/kotlin/util/App.kt
+++ src/main/kotlin/util/App.kt
@@ -1,8 +1,8 @@
 package util
 
 object App {
-    val androidHomeGatePath = "${System.getProperty("user.dir")}/apps/homegate/android/builds/4524.apk"
-    val iOSdHomeGatePath = "${System.getProperty("user.dir")}/apps/homegate/android/builds/homegate.ch.app.zip"
+    val androidHomeGatePath = "${System.getProperty("user.dir")}/apps/wl/android/builds/"
+    val iOSdHomeGatePath = "${System.getProperty("user.dir")}/apps/wl/iOS/builds/"
 
 }
 
